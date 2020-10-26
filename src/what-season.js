c9b5ea0a19f6021cabd2a7ed140d4c2921e4a1c4
Approxomiron <nzubtsov194@gmail.com>
2020-10-26 16:36:57 +0300

    feat: season is done

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
- 
+  if(date === undefined) return 'Unable to determine the time of year!'
+  if(!date.getTime()) throw new Error();
+  month = date.getMonth()
+  if (month == 11 || month == 0 || month == 1) {
+    return ('winter')
+  }
+  else if (month >= 2 && month <= 4) {
+    return ('spring')
+  }
+  else if (month >= 5 && month <= 7) {
+    return ('summer')
+  }
+ else {
+    return ('autumn')
+  }
+
 };
